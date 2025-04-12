2996bf377cb05f3d9ddd090e25250c56f4dcf51c
Vishnu Mouli <vishnumouli0@gmail.com>
2025-04-12 11:57:09 +0530

    feat: expert routes setup for analytics

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,8 +1,12 @@
+import { accountRouter } from "./router/account-router";
 import { authRouter } from "./router/auth-router";
+import { expertRouter } from "./router/expert-router";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
+  account: accountRouter,
+  expert: expertRouter,
 });
 
 // export type definition of API
