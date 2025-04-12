bacbde3eb05820025b378dfa12fc567a0efdb42a
Vishnu Mouli <vishnumouli0@gmail.com>
2025-04-12 12:57:17 +0530

    fix: updated app router with user

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -2,6 +2,7 @@ import { accountRouter } from "./router/account-router";
 import { authRouter } from "./router/auth-router";
 import { expertRouter } from "./router/expert-router";
 import { healthCheckRouter } from "./router/health-check-router";
+import { userRouter } from "./router/user-router";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
@@ -9,6 +10,7 @@ export const appRouter = createTRPCRouter({
   account: accountRouter,
   expert: expertRouter,
   health: healthCheckRouter,
+  user: userRouter,
 });
 
 // export type definition of API
