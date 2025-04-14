85400d2f6d01b59e59167d39f3c63bb4f45ea2af
Vishnu Mouli <vishnumouli0@gmail.com>
2025-04-14 22:13:05 +0530

    fix: prisma client fix

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,19 +1,2 @@
-import { PrismaClient } from "../generated/client";
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const db = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
-
-// export const createPool = () => {
-//   return new PrismaClient({
-//     datasources: {
-//       db: {
-//         url: `${process.env.DATABASE_URL}&connection_limit=20`,
-//       },
-//     },
-//   });
-// };
-
 export * from "../generated/client";
+export { db } from "./client";
