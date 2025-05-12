6c6073e8915dd54c110b612a41df20e1cd4726ee
Vishnu Mouli <vishnumouli0@gmail.com>
2025-05-12 23:29:31 +0530

    feat: cron job to update call status

--- apps/web/src/app/api/cron/calls/route.ts
+++ apps/web/src/app/api/cron/calls/route.ts
@@ -1,5 +1,22 @@
-import { NextResponse } from "next/server";
+import { db } from "@repo/db";
 
-export function GET() {
-  return NextResponse.json({ message: "Hello, world!" });
+export async function GET() {
+  try {
+    await db.call.updateMany({
+      where: {
+        status: "SCHEDULED",
+        endedAt: {
+          lte: new Date(),
+        },
+      },
+      data: {
+        status: "COMPLETED",
+      },
+    });
+
+    return new Response("Calls updated successfully", { status: 200 });
+  } catch (error) {
+    console.log("ERROR", error);
+    return new Response("Error", { status: 500 });
+  }
 }
